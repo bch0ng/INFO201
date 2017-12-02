17e7d1f715c0eda4b1e7a3b4a6dee3426035f98e
Satvik Shukla <satvikshukla9@gmail.com>
2017-12-02 00:55:56 -0800

    remove NA avg values

--- scripts/education_completed_data_wrangling.R
+++ scripts/education_completed_data_wrangling.R
@@ -2,8 +2,8 @@ FormatData <- function(data) {
   data.format <- data[c(1:225),c(1:80)]
   col.names.temp <- vector()
   for (i in colnames(data.format)) {
-    if (!(grepl('Footnotes', i) | grepl('Type', i)))
-      col.names.temp <- c(col.names.temp, i)
+	if (!(grepl('Footnotes', i) | grepl('Type', i)))
+	  col.names.temp <- c(col.names.temp, i)
   }
   data.format <- select(data.format, col.names.temp)
   data.format <- data.format[,-c(3:5)]
@@ -18,7 +18,7 @@ TransposeData <- function(data) {
   data.transpose <- cbind(year = c(1990:2014), data.transpose)
   rownames(data.transpose) <- c(1:25)
   for (i in colnames(data.transpose)) {
-    data.transpose[[i]] <- as.numeric(as.character(data.transpose[[i]]))
+	data.transpose[[i]] <- as.numeric(as.character(data.transpose[[i]]))
   }
   return(data.transpose)
 }
@@ -26,15 +26,19 @@ TransposeData <- function(data) {
 CalculateAvgChange <- function(data.transpose) {
   avg.change <- vector()
   for (i in colnames(data.transpose)[2:ncol(data.transpose)]) {
-    # If country <2 non-NA values then they will have an NA average change
-    if (all(is.na(data.transpose[[i]]))) {
-      change <- NA
-    } else {
-      change <- coef(lm(data.transpose[[i]] ~ year, data = data.transpose, na.action = 'na.exclude'))[[2]]
-    }
-    avg.change <- c(avg.change, change)
+	# If country <2 non-NA values then they will have an NA average change
+	if (all(is.na(data.transpose[[i]]))) {
+	  change <- NA
+	} else {
+	  change <- coef(lm(data.transpose[[i]] ~ year, data = data.transpose, na.action = 'na.exclude'))[[2]]
+	}
+	avg.change <- c(avg.change, change)
   }
   # 40 countries have NA for average rate of change
   return(avg.change)
 }
 
+RemoveNA <- function(input.data) {
+	output.data <- input.data[!(is.na(input.data$avg.change) | input.data$avg.change==""), ]
+	return(output.data)
+}
--- scripts/server.R
+++ scripts/server.R
@@ -11,6 +11,7 @@ both.sex <- FormatData(read.csv('../data/DisplayByIndicator.csv', stringsAsFacto
 both.sex.transpose <- TransposeData(both.sex)
 avg.change <- CalculateAvgChange(both.sex.transpose)
 both.sex$avg.change <- avg.change
+both.sex.no.na <- RemoveNA(both.sex)
 # Arranged for most positive/negative change to be the top-most value
 both.sex.neg.change <- both.sex %>%
                         filter(avg.change < 0) %>% 
