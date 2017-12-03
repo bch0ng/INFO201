d89cf2df73ec2b05d3402c57866f649b9dccb2fa
bch0ng <bchong@uw.edu>
2017-12-03 14:25:16 -0800

    Adds navbar

--- scripts/ui.R
+++ scripts/ui.R
@@ -1,29 +1,28 @@
 library(shiny)
 
 
-shinyUI(fluidPage(
+shinyUI(navbarPage('Millenium Development Goals: Education',
+  fluid = TRUE,
   theme = 'styles.css',
-  titlePanel("Millenium Development Goals: Education"),
-  h4('By Patricia Au, Brandon Chong, Jisoo Kim, Satvik Shukla, Jion Yi'),
-  sidebarLayout(
-    mainPanel(
-      h3(id = 'tableHeader', 'Primary School Completion'),
-      tableOutput('table')
-    ),
-    sidebarPanel(
-      id = 'sidebar',
-      
-      
-      
-      # Table Widgets
-      radioButtons('sex', 'Sex',
-                   choices = list("Both" = 1, "Male" = 2, "Female" = 3), 
-                   selected = 1),
-      radioButtons('arrange.by', 'Arrange By',
-                   choices = list("Descending" = 1, "Ascending" = 2), 
-                   selected = 1),
-      sliderInput("table.max", 'Primary Completed Rate', min = 30, 
-                  max = 115, value = c(30,115))
+  tabPanel('Table',
+    h4('By Patricia Au, Brandon Chong, Jisoo Kim, Satvik Shukla, Jion Yi'),
+    sidebarLayout(
+      mainPanel(
+        h3(id = 'tableHeader', 'Primary School Completion'),
+        tableOutput('table')
+      ),
+      sidebarPanel(
+        id = 'tableSidebar',
+        # Table Widgets
+        radioButtons('sex', 'Sex',
+                     choices = list("Both" = 1, "Male" = 2, "Female" = 3), 
+                     selected = 1),
+        radioButtons('arrange.by', 'Arrange By',
+                     choices = list("Descending" = 1, "Ascending" = 2), 
+                     selected = 1),
+        sliderInput("table.max", 'Primary Completed Rate', min = 30, 
+                    max = 115, value = c(30,115))
+      )
     )
   )
 ))
