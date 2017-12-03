2bf9437020dcaa7fe773698155d19be337c725ab
Patricia Au <patau@uw.edu>
2017-12-03 13:40:42 -0800

    Add function for getting data by sex

--- scripts/completion_scatter.R
+++ scripts/completion_scatter.R
@@ -4,4 +4,26 @@ source('./scripts/education_completed_data_wrangling.R')
 
 girls.completion <- FormatData(read.csv('./data/Completion_girls.csv', stringsAsFactors = FALSE))
 boys.completion <- FormatData(read.csv('./data/Completion_boys.csv', stringsAsFactors = FALSE))
+both.completion <- FormatData(read.csv('./data/Completion_both.csv', stringsAsFactors = FALSE))
+
+#Calculate average value for both boys and girls separately
+
+#Function: calculate average boys and girls for chosen country
+
+
+#Function: filter for country in dataset
+GetCountryData <- function(country, sex) {
+  data;
+  if (sex == "girls") {
+    data <- girls.completion
+  } else if (sex == "boys") {
+    data <- boys.completion
+  } else {
+    data <- both.completion
+  }
+  return(data %>% filter(Country == country))
+}
+
+#View(GetCountryData("Algeria", "both"))
+#View(GetCountryData("Algeria", "girls"))
 
