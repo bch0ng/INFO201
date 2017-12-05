b2690759890d23561ef15c7bdfc0c37661cfdc74
Satvik Shukla <satvikshukla9@gmail.com>
2017-12-05 11:28:56 -0800

    change paths

--- scripts/map.R
+++ scripts/map.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(plotly)
 library(tidyr)
 library(xlsx)
-source('scripts/education_completed_data_wrangling.R')
+source('education_completed_data_wrangling.R')
 
 # read in & wrangle data 
 # df1 <- read.csv('scripts/abc.csv')
@@ -14,7 +14,7 @@ source('scripts/education_completed_data_wrangling.R')
 # colnames(df2) <- c('country.name', 'country.code.3letters')
 # df <- left_join(df1, df2, by = 'country.name')
 # df <- filter(df, !is.na(country.code.3letters)) 
-df <- read.csv('data/abcd.csv', stringsAsFactors = FALSE)
+df <- read.csv('../data/abcd.csv', stringsAsFactors = FALSE)
 
 # light grey boundaries
 l <- list(color = toRGB('grey'), width = 0.5)
