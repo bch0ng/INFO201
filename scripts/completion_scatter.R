90699b9f9f1843bef9272c6ac6bbf3d9ce3fa62f
Jisoo Kim <Jisoo@Jisoos-MacBook-Air.local>
2017-12-06 07:13:15 -0800

    Scatterplot edit

--- scripts/completion_scatter.R
+++ scripts/completion_scatter.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(plotly)
-source('./scripts/education_completed_data_wrangling.R')
+setwd("~/Desktop/INFO201/INFO201")
+source("./scripts/education_completed_data_wrangling.R")
 
 girls.completion <- FormatData(read.csv('./data/Completion_girls.csv', stringsAsFactors = FALSE))
 boys.completion <- FormatData(read.csv('./data/Completion_boys.csv', stringsAsFactors = FALSE))
@@ -9,7 +10,9 @@ both.completion <- FormatData(read.csv('./data/Completion_both.csv', stringsAsFa
 #Girls and boys average completion rates; for trend line
 girls.avg <- girls.completion[c(3:length(girls.completion))] %>% 
   summarize_all(funs(mean(., na.rm=TRUE)))
-boys.avg <- girls.completion[c(3:length(girls.completion))] %>% 
+boys.avg <- boys.completion[c(3:length(boys.completion))] %>% 
+  summarize_all(funs(mean(., na.rm=TRUE)))
+both.avg <- both.completion[c(3:length(both.completion))] %>% 
   summarize_all(funs(mean(., na.rm=TRUE)))
 
 #Function: filter for country in dataset
@@ -24,6 +27,27 @@ GetCountryData <- function(country, sex) {
   }
   return(data %>% filter(Country == country))
 }
+
+trans <- as.data.frame(t(GetCountryData("Afghanistan", "girls")))
+into.column <- data.frame(year = row.names(trans), trans, row.names = NULL)
+
+CountryData <- function(country, sex) {
+  trans <- as.data.frame(t(GetCountryData(country, sex)))
+  into.column <- data.frame(year = row.names(trans), trans, row.names = NULL)
+  return(into.column)
+}
+
+Scatter <- function(country, sex) {
+  choice <- CountryData(country, sex)
+  scatterplot <- plot_ly(choice, type="scatter", x = ~choice$year[3:27], y = ~paste0(choice, "$V1[3:27]")) %>% 
+    layout(title = 'Average per Year',
+           xaxis = list(title = 'Year',
+                        zeroline = TRUE
+           ),
+           yaxis = list(title = 'Average'
+           ))
+}
+
 
 # fit <- lm(price ~ carat, data = df)
 # plot_ly(df1, x = carat, y = price, mode = "markers") %>% 
