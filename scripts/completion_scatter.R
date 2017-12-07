60e412fc1a271960a509997f005e507493bead7c
Patricia Au <patau@uw.edu>
2017-12-07 03:16:25 -0800

    Changed file paths; tinkered with line and points

--- scripts/completion_scatter.R
+++ scripts/completion_scatter.R
@@ -2,21 +2,14 @@ library(dplyr)
 library(plotly)
 library(shiny)
 
-source("./scripts/education_completed_data_wrangling.R")
+source("education_completed_data_wrangling.R")
 
-girls.completion <- FormatData(read.csv('./data/Completion_girls.csv', stringsAsFactors = FALSE))
-boys.completion <- FormatData(read.csv('./data/Completion_boys.csv', stringsAsFactors = FALSE))
-both.completion <- FormatData(read.csv('./data/Completion_both.csv', stringsAsFactors = FALSE))
+girls.completion <- FormatData(read.csv('../data/Completion_girls.csv', stringsAsFactors = FALSE))
+boys.completion <- FormatData(read.csv('../data/Completion_boys.csv', stringsAsFactors = FALSE))
+both.completion <- FormatData(read.csv('../data/Completion_both.csv', stringsAsFactors = FALSE))
 
-#Girls and boys average completion rates; for trend line
-# girls.avg <- girls.completion[c(3:length(girls.completion))] %>% 
-#   summarize_all(funs(mean(., na.rm=TRUE)))
-# girls.avg = as.data.frame(t(girls.avg))
-# girls.avg <- data.frame(year = row.names(girls.avg), girls.avg, row.names = NULL)
-# boys.avg <- boys.completion[c(3:length(boys.completion))] %>% 
-#   summarize_all(funs(mean(., na.rm=TRUE)))
-# both.avg <- both.completion[c(3:length(both.completion))] %>% 
-#   summarize_all(funs(mean(., na.rm=TRUE)))
+#For ui.R dropdown
+dropdown.choices <- both.completion$Country
 
 GetAllCountriesAvg <- function(sex) {
   if (sex == "girls") {
@@ -44,28 +37,42 @@ GetCountryData <- function(country, sex) {
   } else {
     data <- both.completion
   }
+  
   country.data <- data %>% filter(Country == country) %>% 
     select(-CountryCode, -Country)
   trans <- as.data.frame(t(country.data))
   country.data <- data.frame(year = row.names(trans), trans, row.names = NULL)
   country.data$year <- gsub('X', '', country.data$year)
+  country.data <- filter(country.data, !is.na(V1)) 
   return(country.data)
 }
 
-all.data <- GetAllCountriesAvg("girls")
-dropdown.choices <- both.completion$Country
+
+#Scatter("Algeria", "girls")
+# Scatter("Australia", "girls")
+# GetCountryData("Australia", "girls")
+# GetCountryData("Algeria", "girls")
 
 Scatter <- function(country, sex) {
   d <- GetCountryData(country, sex)
   all.data <- GetAllCountriesAvg(sex)
+  #Plot graph with only average line
   scatterplot <- plot_ly() %>% 
-    add_trace(data=d, x = ~d$year, y = ~d$V1, type="scatter", mode="markers") %>% 
     layout(title = paste(country, "Average per Year"),
-           xaxis = list(title = 'Year', zeroline = TRUE),
+           xaxis = list(title = 'Year', zeroline = TRUE, tickangle = -45),
            yaxis = list(title = 'Average'),
            showlegend = FALSE) %>% 
-    add_trace(data=all.data, x = ~all.data$year, y = ~all.data$V1, type="scatter", mode = "lines")
+    #Adds average line
+    add_trace(data=all.data, x = ~all.data$year, y = ~all.data$V1, 
+              type="scatter", mode = "lines", text = ~paste("Worldwide: ", format(round(V1, 2), nsmall = 2)))
+  if (nrow(d) > 0) {
+    #Only adds country's value if there are some values (not all null)
+    scatterplot <- scatterplot %>% add_trace(data=d, x = ~d$year, 
+                                             y = ~d$V1, type="scatter", 
+                                             mode="markers",
+                                             text = ~paste(country, ": ", V1,
+                                                           sep = ""))
+  }
   return(scatterplot)
 }
 
-typeof(Scatter("Algeria", "girls"))
